d5aa0e8893cc077034627ea5c0d15f6a5bd73199
Samson Ng <ngsamson38@gmail.com>
2019-03-17 02:42:50 +0800

    dictionary method

--- bookkepping.py
+++ bookkepping.py
@@ -1,15 +1,11 @@
 #encoding = utf-8
-#two dimentional list 二維清單
-products = []
+#dictionary 字典
+
+products = {}
 while True:
     name = input('請輸入商品名稱: ')
     if name == 'q':
         break
     price = input('請輸入商品價格: ')
-    p = []
-    p.append(name)
-    p.append(price)
-    products.append(p)
+    products[name] = price
 print(products)
-
-print(products[0][0])
