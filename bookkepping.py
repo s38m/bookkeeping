442c787e3ccc38df5c30b1fe7c964d6a34ece17c
s38m <42419897+s38m@users.noreply.github.com>
2019-03-17 19:11:30 +0800

    add encoding='utf-8'

--- bookkepping.py
+++ bookkepping.py
@@ -13,6 +13,7 @@ print(products)
 for p in products:
     print(p[0], '的價格是', p[1])
 
-with open('products.csv', 'w') as f:
+with open('products.csv', 'w', encoding='utf-8') as f:
+    f.write('商品名稱, 商品價格\n')
     for p in products:
         f.write(p[0] + ',' + p[1] + '\n')
