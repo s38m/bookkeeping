ce28f72bf8bfd792553f9b338e42518d5d99c875
Samson Ng <ngsamson38@gmail.com>
2019-03-18 22:41:25 +0800

    add for loop tp avoid output header

--- bookkeeping_read.py
+++ bookkeeping_read.py
@@ -1,5 +1,7 @@
 with open('products.csv', 'r', encoding='utf-8') as f:
     for line in f:
+        for '商品,價格' in line:
+            continue
         name, price = line.strip().split(',') 
         products.append([name, price])
 print(products)
