590f479cd7c5600b448f6775ca82956db6fb2498
Samson Ng <ngsamson38@gmail.com>
2019-03-19 14:41:00 +0800

    add os.path.isfile

--- bookkeeping_read.py
+++ bookkeeping_read.py
@@ -1,17 +1,15 @@
 import os
 products = []
-'''
+
 if os.path.isfile('products.csv'):
     print('File exist')
-'''    
-#讀取檔案
+    #讀取檔案
     with open('products.csv', 'r', encoding='utf-8') as f:
         for line in f:
             if '商品,價格' in line:
                 continue
             name, price = line.strip().split(',') 
             products.append([name, price])
+    print(products)
 else:
     print('File not found')
-
-print(products)
