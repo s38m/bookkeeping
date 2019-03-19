8370c22e00c21923d8a20cd3a1ed8c54f53fbc9a
Samson Ng <ngsamson38@gmail.com>
2019-03-19 14:40:00 +0800

    add os.path.isfile

--- bookkeeping_read.py
+++ bookkeeping_read.py
@@ -1,7 +1,17 @@
-with open('products.csv', 'r', encoding='utf-8') as f:
-    for line in f:
-        for '商品,價格' in line:
-            continue
-        name, price = line.strip().split(',') 
-        products.append([name, price])
+import os
+products = []
+'''
+if os.path.isfile('products.csv'):
+    print('File exist')
+'''    
+#讀取檔案
+    with open('products.csv', 'r', encoding='utf-8') as f:
+        for line in f:
+            if '商品,價格' in line:
+                continue
+            name, price = line.strip().split(',') 
+            products.append([name, price])
+else:
+    print('File not found')
+
 print(products)
