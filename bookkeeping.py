217b4fe46b19cf50fd5babf46096461798bf206f
Samson Ng <ngsamson38@gmail.com>
2019-03-19 14:39:25 +0800

    add os.path.isfile

--- bookkeeping.py
+++ bookkeeping.py
@@ -1,7 +1,23 @@
 #encoding = utf-8
 #two dimentional list 二維清單
 
+import os
 products = []
+
+if os.path.isfile('products.csv'):
+    print('File exist')
+    #讀取檔案
+    with open('products.csv', 'r', encoding='utf-8') as f:
+        for line in f:
+            if '商品,價格' in line:
+                continue
+            name, price = line.strip().split(',') 
+            products.append([name, price])
+    print(products)
+else:
+    print('File not found')
+
+#讓使用者輸入
 while True:
     name = input('請輸入商品名稱: ')
     if name == 'q':
@@ -13,7 +29,9 @@ print(products)
 for p in products:
     print(p[0], '的價格是', p[1])
 
+#印出清單
 with open('products.csv', 'w', encoding='utf-8') as f:
     f.write('商品名稱,商品價格\n')
     for p in products:
         f.write(p[0] + ',' + p[1] + '\n')
+
