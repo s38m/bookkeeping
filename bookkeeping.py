e5f53a8aa8e28bd4a4b75d908545047b85990c65
Samson Ng <ngsamson38@gmail.com>
2019-03-20 01:04:36 +0800

    refactor

--- bookkeeping.py
+++ bookkeeping.py
@@ -3,19 +3,13 @@
 
 import os
 
-def read_file(filename):#讀取檔案
-    products = []
-    if os.path.isfile(filename):
-        print('已找到檔案')
-        with open(filename, 'r', encoding='utf-8') as f:
-            for line in f:
-                if '商品,價格' in line:
-                    continue
-                name, price = line.strip().split(',') 
-                products.append([name, price])
-        print(products)
-    else:
-        print('系統找不到對應檔案')
+def read_file(filename, products):#讀取檔案
+    with open(filename, 'r', encoding='utf-8') as f:
+        for line in f:
+            if '商品名稱,商品價格' in line:
+                continue
+            name, price = line.strip().split(',') 
+            products.append([name, price])   
     return products
 
 def user_input(products):#讓使用者輸入
@@ -38,7 +32,17 @@ def write_file(filename, products):#寫入印出所有購買記錄
         for p in products:
             f.write(p[0] + ',' + p[1] + '\n')
 
-products = read_file('products.csv')
-products = user_input(products)
-print_product(products)
-write_file('products.csv', products)
+def main():
+    products = []
+    filename = input('請輸入檔案名稱: ')
+    if os.path.isfile(filename):
+        print('已找到檔案')
+        products = read_file(filename, products)
+    else:
+        print('對應檔案不存在')
+    
+    products = user_input(products)
+    print_product(products)
+    write_file(filename, products)
+
+main()
